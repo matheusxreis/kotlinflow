e9be4d6656049cf4b539cc5d93637bcb3e92baaa
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 11:43:52 -0300

    refactor: returning messages direct from repository instead of view model

--- app/src/main/java/com/matheusxreis/kotlinflow/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/matheusxreis/kotlinflow/viewmodels/MainViewModel.kt
@@ -14,12 +14,7 @@ class MainViewModel constructor(private val repository: MessageRepository): View
 
     fun observeMessages(){
 
-        message.value = listOf<Message>(
-            Message(text = "hello", "me", "you", 1212121212),
-            Message(text = "how are you?", "me", "you", 1212121212),
-            Message(text = "i'm missing you", "me", "you", 1212121212),
-            Message(text = "can we see each other some day?", "me", "you", 1212121212),
-        )
+        message.value = repository.observeMessages()
     }
 
     class MainViewModelFactory(
