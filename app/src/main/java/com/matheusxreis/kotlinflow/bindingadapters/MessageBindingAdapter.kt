b4ca32561500ea3b14e83b4099434df27f770dab
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 12:27:18 -0300

    feat: changing horizontal bias of layout row with isMessageMine adapter

--- app/src/main/java/com/matheusxreis/kotlinflow/bindingadapters/MessageBindingAdapter.kt
+++ app/src/main/java/com/matheusxreis/kotlinflow/bindingadapters/MessageBindingAdapter.kt
@@ -1,6 +1,9 @@
 package com.matheusxreis.kotlinflow.bindingadapters
 
+import android.app.ActionBar.LayoutParams
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.google.android.material.card.MaterialCardView
@@ -16,9 +19,14 @@ class MessageBindingAdapter {
             if(from === "me"){
                 when(view){
                     is MaterialCardView -> {
+
+                        val params = view.layoutParams as ConstraintLayout.LayoutParams
+                        params.horizontalBias = 1f
+                        view.layoutParams = params
                         view.setCardBackgroundColor(
                             ContextCompat.getColor(view.context, R.color.teal_700)
                         )
+
                     }
                 }
             }
