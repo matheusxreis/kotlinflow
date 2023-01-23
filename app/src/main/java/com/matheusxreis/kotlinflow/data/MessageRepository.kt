1f2ce3b9e7553321f8eddde58309f675db216899
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 15:14:02 -0300

    chore: mocked more messages

--- app/src/main/java/com/matheusxreis/kotlinflow/data/MessageRepository.kt
+++ app/src/main/java/com/matheusxreis/kotlinflow/data/MessageRepository.kt
@@ -12,35 +12,18 @@ class MessageRepository {
         Message(text = "hello", from = "you", to = "me", sentAt = 1212121212),
         Message(text = "how are you?", from = "you", to = "me", sentAt = 1212121212),
         Message(text = "i'm missing you", from = "you", to = "me", sentAt = 1212121212),
-        Message(
-            text = "can we see each other some day?",
-            from = "you",
-            to = "me",
-            sentAt = 1212121212
-        ),
-        Message(
-            text = "hi, i dont know if it is a good idea",
-            from = "me",
-            to = "you",
-            sentAt = 129182182
-        ),
+        Message(text = "can we see each other some day?", from = "you", to = "me", sentAt = 1212121212),
+        Message(text = "hi, i dont know if it is a good idea", from = "me", to = "you", sentAt = 129182182),
         Message(text = "why not?", from = "you", to = "me", sentAt = 192891829122),
         Message(text = "i think you know why", from = "me", to = "you", sentAt = 1289128912),
-        Message(
-            text = "let me the chance of apologize me",
-            from = "you",
-            to = "me",
-            sentAt = 192871928192
-        ),
+        Message(text = "let me the chance of apologize me", from = "you", to = "me", sentAt = 192871928192),
         Message(text = "please...", from = "you", to = "me", sentAt = 192871928192),
         Message(text = "ok... where?", from = "me", to = "you", sentAt = 1289128912),
-        Message(
-            text = "same place, you won't regret",
-            from = "you",
-            to = "me",
-            sentAt = 192871928192
+        Message(text = "same place, you won't regret", from = "you", to = "me", sentAt = 192871928192),
+        Message(text = "ok... when?", from = "me", to = "you", sentAt = 1289128912),
+        Message(text = "tomorrow, after dinner, ok?", from = "you", to = "me", sentAt = 192871928192),
+        Message(text = "ok, see u", from = "me", to = "you", sentAt = 1289128912),
         )
-    )
 
     fun observeMessages(): Flow<Message> {
         return flow {
