72261a0ec098825b51d0156723f59da5a94dc71b
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 11:04:30 -0300

    feat: binding main activity

--- app/src/main/java/com/matheusxreis/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/matheusxreis/kotlinflow/MainActivity.kt
@@ -2,10 +2,14 @@ package com.matheusxreis.kotlinflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.matheusxreis.kotlinflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
