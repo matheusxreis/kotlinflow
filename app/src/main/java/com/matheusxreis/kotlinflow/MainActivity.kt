39a5f4206a565ebf181f7543cd90adb23708db50
Matheus Reis <matheus.reis081@gmail.com>
2023-01-23 11:34:32 -0300

    feat: created funcitons to set up and populate rv

--- app/src/main/java/com/matheusxreis/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/matheusxreis/kotlinflow/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.matheusxreis.kotlinflow
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.matheusxreis.kotlinflow.adapters.MessageAdapter
 import com.matheusxreis.kotlinflow.databinding.ActivityMainBinding
 import com.matheusxreis.kotlinflow.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel;
+    private lateinit var binding:ActivityMainBinding;
+    private val messageAdapter by lazy {
+        MessageAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupRecyclerView()
+        populateRecyclerView()
+    }
+
+    fun setupRecyclerView(){
+            binding.messagesRv.adapter = messageAdapter;
+            binding.messagesRv.layoutManager = LinearLayoutManager(applicationContext)
+    }
+
+    fun populateRecyclerView(){
+        mainViewModel.message.observe(this) {
+            messageAdapter.setData(it)
+        }
     }
 }
